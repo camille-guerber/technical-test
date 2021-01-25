ab0bcf1b76cbb9d69cb92b57cb5dc8ebfc419511
Thibault HENRY <thibault@fidcar.com>
2021-01-25 21:49:15 +0100

    user entity

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    public function __construct(
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ) {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setEmail('thibault@fidcar.com');
+        $user->setFirstname('Thibault');
+        $user->setLastname('Henry');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            $this->userPasswordEncoder->encodePassword($user, 'VerySecretPassword'),
+        );
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
